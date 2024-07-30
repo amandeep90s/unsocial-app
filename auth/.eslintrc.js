7b888d467900e56fa4eaa937e2a002d36e14b080
Amandeep Singh <programmeramandeep@gmail.com>
2024-07-30 15:38:32 +0530

    fix: eslintrs file configuration issue

--- auth/.eslintrc.js
+++ auth/.eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['airbnb-typescript-prettier'],
-  parserOptions: { ecmaVersion: 2018, sourceType: 'module' },
+  extends: ['airbnb-typescript-prettier', 'prettier'],
+  plugins: ['import', 'prettier', '@typescript-eslint'],
+  parserOptions: { project: './tsconfig.json', tsconfigRootDir: __dirname },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+  },
   settings: {
     react: {
       version: '999.999.999', // Invalid version to prevent detection attempts
