9a69132773be27620da19ef45de50a51c760afbe
Amandeep Singh <programmeramandeep@gmail.com>
2024-07-30 12:19:28 +0530

    feat: added express validator inside signup test

--- auth/src/routes/__tests__/signup.test.ts
+++ auth/src/routes/__tests__/signup.test.ts
@@ -7,4 +7,5 @@ it('should return 405 for non-posts requests to the signup route', () => {
 
 it('should return 422 if the email is not valid', async () => {
   await request(app).post('/api/auth/signup').send({}).expect(422);
+  await request(app).post('/api/auth/signup').send({ email: 'invalidEmail' }).expect(422);
 });
